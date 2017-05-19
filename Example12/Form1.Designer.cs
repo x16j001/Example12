cd948688f715073c2f85f615e3b4a20f7f88d0aa
WIN_NT\x16j001 <x16j001@chiba-fjb.ac.jp>
2017-05-19 12:12:07 +0900

    no message

---
+++ Example12/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Example12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxMonth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnJuge = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxMonth
+            // 
+            this.tbxMonth.Location = new System.Drawing.Point(20, 29);
+            this.tbxMonth.Name = "tbxMonth";
+            this.tbxMonth.Size = new System.Drawing.Size(67, 19);
+            this.tbxMonth.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "月";
+            // 
+            // btnJuge
+            // 
+            this.btnJuge.Location = new System.Drawing.Point(141, 28);
+            this.btnJuge.Name = "btnJuge";
+            this.btnJuge.Size = new System.Drawing.Size(64, 19);
+            this.btnJuge.TabIndex = 2;
+            this.btnJuge.Text = "判定";
+            this.btnJuge.UseVisualStyleBackColor = true;
+            this.btnJuge.Click += new System.EventHandler(this.btnJuge_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(27, 132);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnJuge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxMonth);
+            this.Name = "Form1";
+            this.Text = "月判定";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnJuge;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Example12/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJuge_Click(object sender, EventArgs e)
+        {
+            int month = int.Parse(tbxMonth.Text);
+
+            if (month >= 1 && month <= 12)
+            {
+                if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                    lblResult.Text = "大の月";
+                else
+                    lblResult.Text = "小の月";
+            }
+            else
+                lblResult.Text = "入力エラー";
+        }
+    }
+}
